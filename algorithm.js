17619e0e0be6fba29c41a172b968ea7dad9b14cb
Gabe Rojas-Westall <rojaswestall@gmail.com>
2017-07-31 16:11:37 -0500

    Added more api calls

--- algorithm.js
+++ algorithm.js
@@ -1,7 +1,7 @@
 // author: Gabe Rojas-Westall (GitHub: rojaswestall)
 // Messing around with the gdax api
 var Gdax = require('gdax');
-var Config = require('./config.js');
+var Config = require('./grwsandboxconfig.js');
 
 
 // Public endpoints from the gdax api
@@ -28,23 +28,80 @@ var authedClient = new Gdax.AuthenticatedClient(key, b64secret, passphrase, sand
 // var callback = function(err, response, data) {
 // 
 // };
+
+////////////// NEED TO WRITE ERROR HANDLERS //////////////
+// 400	Bad Request – Invalid request format
+// 401	Unauthorized – Invalid API Key
+// 403	Forbidden – You do not have access to the requested resource
+// 404	Not Found
+// 500	Internal Server Error – We had a problem with our server
+
 var accounts = function(err, response, data) {
-	console.log('Below is the data that getAccount is giving through the private endpoint:');
-	console.log(data[1].currency);
+	console.log('Balance - bitcoin wallet:');
+	// console.log(data);
+	console.log(data[0].balance);
 	// console.log('This is the repsonse:');
 	// console.log(response);
   
 };
-
 authedClient.getAccounts(accounts);
 
-var callback = function(err, response, data) {
-	console.log('getProductTicker is using the public endpoint and giving us info about eth:');
+
+var orderbook = function(err, response, data) {
+	console.log('Order book ETH-USD:');
 	//console.log(response)
-	console.log(data);
+	// console.log(data);
 	// console.log(data.price);
   
 };
+publicClientETH.getProductOrderBook({'level' : 3}, orderbook);
+// Level 3 is only recommended for users wishing to maintain a full real-time order book using the 
+// websocket stream. Abuse of Level 3 via polling will cause your access to be limited or blocked.
 
-publicClientETH.getProductOrderBook(callback);
+var ethticker = function(err, response, data) {
+	console.log('eth ticker:')
+	console.log(data);
+};
+publicClientETH.getProductTicker(ethticker);
+// Snapshot information about the last trade (tick), best bid/ask and 24h volume.
+
+var ethtrades = function(err, response, data) {
+	console.log('eth trades:')
+	console.log(data);
+	console.log(response)
+};
+publicClientETH.getProductTrades(ethtrades);
+// The trade side indicates the maker order side. The maker order is the order that was open on the 
+// order book. buy side indicates a down-tick because the maker was a buy order and their order was 
+// removed. Conversely, sell side indicates an up-tick.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
