a2abc4b3895cccb94ce6e460026091b738a83195
Gabe Rojas-Westall <rojaswestall@gmail.com>
2017-07-31 10:33:56 -0500

    Added a config file and started making private endpoint calls

--- algorithm.js
+++ algorithm.js
@@ -1,20 +1,50 @@
 // author: Gabe Rojas-Westall (GitHub: rojaswestall)
 // Messing around with the gdax api
-
-
-// For now, using the public endpoints from the gdax api
 var Gdax = require('gdax');
-var publicClient = new Gdax.PublicClient('ETH-USD', 'https://api.gdax.com');
+var Config = require('./config.js');
+
+
+// Public endpoints from the gdax api
+var publicClientETH = new Gdax.PublicClient('ETH-USD', 'https://api.gdax.com');
+var publicClientBTC = new Gdax.PublicClient('BTC-USD', 'https://api.gdax.com');
+
+var key = Config.key;
+var b64secret = Config.b64secret;
+var passphrase = Config.passphrase;
+var btcaddress = Config.btcaddress;
+
+var apiURI = 'https://api.gdax.com';
+var sandboxURI = 'https://api-public.sandbox.gdax.com';
+
+// Private enpoints
+var authedClient = new Gdax.AuthenticatedClient(key, b64secret, passphrase, sandboxURI);
+// var authedClient = new Gdax.AuthenticatedClient(key, b64secret, passphrase, apiURI);
 
 // All API methods are callback based. 
 // This callback will be passed directly to the underlying request library's request method. 
 // err will be either null or an Error. 
 // response will be a generic HTTP response abstraction created by the request library. 
 // data will be the result of JSON-decoding the server's response, or null if the response was not parseable.
-var callback = function(err, response, data) {
-	console.log('getProductTicker is using the callback')
-	console.log(response)
+// var callback = function(err, response, data) {
+// 
+// };
+var accounts = function(err, response, data) {
+	console.log('Below is the data that getAccount is giving through the private endpoint:');
+	console.log(data[1].currency);
+	// console.log('This is the repsonse:');
+	// console.log(response);
   
 };
 
-publicClient.getProductTicker(callback);
+authedClient.getAccounts(accounts);
+
+var callback = function(err, response, data) {
+	console.log('getProductTicker is using the public endpoint and giving us info about eth:');
+	//console.log(response)
+	console.log(data);
+	// console.log(data.price);
+  
+};
+
+publicClientETH.getProductOrderBook(callback);
+
---
+++ config.js
@@ -0,0 +1,12 @@
+// Private Keys and Stuff. DO NOT SHARE YOURS WITH ANYONE!!!!
+
+// Enter your info here:
+var key = '';
+var b64secret = '';
+var passphrase = '';
+var btcaddress = '';
+
+exports.key = key;
+exports.b64secret = b64secret;
+exports.passphrase = passphrase;
+exports.btcaddress = btcaddress;
