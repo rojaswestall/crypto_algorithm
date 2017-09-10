976f4c99ddf794b32173b494646bc0061d779cd5
Gabe Rojas-Westall <rojaswestall@gmail.com>
2017-09-10 16:08:08 -0500

    Promises are working to fill the bitfinexPriceList

--- bitfinexapi.js
+++ bitfinexapi.js
@@ -9,51 +9,51 @@ var bitfinexPriceList = {
 	'NEO': 0,
 }
 
-setPrices(function(msg) {
-	// bitfinexPriceList.coin = price;
-	console.log(msg);
-	console.log(bitfinexPriceList);
+// setPrices(function(msg) {
+// 	// bitfinexPriceList.coin = price;
+// 	console.log(msg);
+// 	console.log(bitfinexPriceList);
 	
-});
+// });
 
-function setPrices(callback) {
-	request.get(url + "/pubticker/btcusd", function(error, response, body) {
-  		if (error) {
-  			console.log(error);
-  		}
-  		var btcPrice = JSON.parse(body).last_price;
-  		bitfinexPriceList.BTC = btcPrice;
+// function setPrices(callback) {
+// 	request.get(url + "/pubticker/btcusd", function(error, response, body) {
+//   		if (error) {
+//   			console.log(error);
+//   		}
+//   		var btcPrice = JSON.parse(body).last_price;
+//   		bitfinexPriceList.BTC = btcPrice;
 
-    	request.get(url + "/pubticker/ethusd", function(error, response, body) {
-  			if (error) {
-  				console.log(error);
-  			}
-	  		var ethPrice = JSON.parse(body).last_price;
-	  		bitfinexPriceList.ETH = ethPrice;
+//     	request.get(url + "/pubticker/ethusd", function(error, response, body) {
+//   			if (error) {
+//   				console.log(error);
+//   			}
+// 	  		var ethPrice = JSON.parse(body).last_price;
+// 	  		bitfinexPriceList.ETH = ethPrice;
 
-		  	request.get(url + "/pubticker/ltcusd", function(error, response, body) {
-		  		if (error) {
-		  			console.log(error);
-		  		}
-			  	var ltcPrice = JSON.parse(body).last_price;
-			  	bitfinexPriceList.LTC = ltcPrice;
+// 		  	request.get(url + "/pubticker/ltcusd", function(error, response, body) {
+// 		  		if (error) {
+// 		  			console.log(error);
+// 		  		}
+// 			  	var ltcPrice = JSON.parse(body).last_price;
+// 			  	bitfinexPriceList.LTC = ltcPrice;
 
-			  	request.get(url + "/pubticker/neousd", function(error, response, body) {
-			  		if (error) {
-			  			console.log(error);
-			  		}
-				  	var neoPrice = JSON.parse(body).last_price;
-				  	bitfinexPriceList.NEO = neoPrice;	
-				  	setTimeout(function() {
-			        	callback('From Callback Method');
-			    	}, 0);	  		
-				})		  		
-			})
-		})
-	})
-};
+// 			  	request.get(url + "/pubticker/neousd", function(error, response, body) {
+// 			  		if (error) {
+// 			  			console.log(error);
+// 			  		}
+// 				  	var neoPrice = JSON.parse(body).last_price;
+// 				  	bitfinexPriceList.NEO = neoPrice;	
+// 				  	callback('From Callbacks:')  		
+// 				})		  		
+// 			})
+// 		})
+// 	})
+// };
 
-
+// setTimeout(function() {
+// 	callback('From Callback Method');
+// }, 0);	
 
 
 
@@ -65,23 +65,76 @@ var getBitcoinPrice = new Promise(function(resolve,reject){
   		}
   		else {
   			var btcPrice = JSON.parse(body).last_price;
-  			resolve(btcPrice);
+  			// var btcPrice = response;
+  			bitfinexPriceList.BTC = btcPrice;
+  			resolve({'BTC': btcPrice});
   		}
 	})
 });
 
-getBitcoinPrice.then(function(btcPrice){
-	bitfinexPriceList.BTC = btcPrice;
-	console.log('From Promise');
+var getEthereumPrice = new Promise(function(resolve,reject){
+	request.get(url + "/pubticker/ethusd", function(error, response, body) {
+  		if (error) {
+  			reject(error);
+  		}
+  		else {
+  			var ethPrice = JSON.parse(body).last_price;
+  			bitfinexPriceList.ETH = ethPrice;
+  			resolve({'ETH': ethPrice});
+  		}
+	})
+});
+
+var getLitecoinPrice = new Promise(function(resolve,reject){
+	request.get(url + "/pubticker/ltcusd", function(error, response, body) {
+  		if (error) {
+  			reject(error);
+  		}
+  		else {
+  			var ltcPrice = JSON.parse(body).last_price;
+  			bitfinexPriceList.LTC = ltcPrice;
+  			resolve({'LTC': ltcPrice});
+  		}
+	})
+});
+
+var getNeoPrice = new Promise(function(resolve,reject){
+	request.get(url + "/pubticker/neousd", function(error, response, body) {
+  		if (error) {
+  			reject(error);
+  		}
+  		else {
+  			var neoPrice = JSON.parse(body).last_price;
+  			bitfinexPriceList.NEO = neoPrice;
+  			resolve({'NEO': neoPrice});
+  		}
+	})
+});
+
+Promise.all([getBitcoinPrice, getEthereumPrice, getLitecoinPrice, getNeoPrice]).then(function(info) {
+	console.log(info);
+	console.log('From Promises:');
 	console.log(bitfinexPriceList);
-}).catch(function(error){
-	console.log(error);
 })
 
-// promise.then(function(result) {
-//   console.log(result); // "Stuff worked!"
+
+//////// A COUPLE OF METHODS FOR PROMISES ////////
+
+// getBitcoinPrice.then(function(btcPrice){
+// 	bitfinexPriceList.BTC = btcPrice;
+// 	console.log('From Promise');
+// 	// console.log(symbol);
+// 	console.log(bitfinexPriceList);
+// }).catch(function(error){
+// 	console.log(error);
+// })
+
+// getBitcoinPrice.then(function(btcPrice) {
+//   	bitfinexPriceList.BTC = btcPrice;
+// 	console.log('From Promise');
+// 	console.log(bitfinexPriceList);
 // }, function(err) {
-//   console.log(err); // Error: "It broke"
+//   console.log(err);
 // });
 
 
